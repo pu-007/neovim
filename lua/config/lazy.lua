94594a32332e247916eba42dad7a23b58e05f779
pu-007 <pu.007@qq.com>
2024-01-23 19:49:56 +0800

    update

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -10,12 +10,9 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
-    -- import any extras modules here
-    -- { import = "lazyvim.plugins.extras.lang.typescript" },
-    -- { import = "lazyvim.plugins.extras.lang.json" },
-    -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
+
     -- import/override with your plugins
-    { import = "plugins" },
+    { "lambdalisue/suda.vim" },
   },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
@@ -33,9 +30,9 @@ require("lazy").setup({
       -- disable some rtp plugins
       disabled_plugins = {
         "gzip",
-        -- "matchit",
-        -- "matchparen",
-        -- "netrwPlugin",
+        "matchit",
+        "matchparen",
+        "netrwPlugin",
         "tarPlugin",
         "tohtml",
         "tutor",
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,7 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
+-- https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+
+local opt = vim.opt
+
+opt.scrolloff = 8 -- Lines of context
+opt.wrap = false -- Disable line wrap
+opt.colorcolumn = { 80 }

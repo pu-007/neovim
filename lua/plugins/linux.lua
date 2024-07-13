2753135ffb07aed79d8d43f6d685401fd80e457e
pu-007 <pu.007@qq.com>
2024-07-13 15:35:59 +0800

    move

--- lua/plugins/linux.lua
+++ lua/plugins/linux.lua
@@ -12,7 +12,42 @@ if vim.loop.os_uname().sysname == "Linux" then
         })
       end,
     },
-
+    {
+      "xiyaowong/transparent.nvim",
+      opts = function()
+        require("transparent").setup({ -- Optional, you don't have to run setup.
+          groups = { -- table: default groups
+            "Normal",
+            "NormalNC",
+            "Comment",
+            "Constant",
+            "Special",
+            "Identifier",
+            "Statement",
+            "PreProc",
+            "Type",
+            "Underlined",
+            "Todo",
+            "String",
+            "Function",
+            "Conditional",
+            "Repeat",
+            "Operator",
+            "Structure",
+            "LineNr",
+            "NonText",
+            "SignColumn",
+            "CursorLine",
+            "CursorLineNr",
+            "StatusLine",
+            "StatusLineNC",
+            "EndOfBuffer",
+          },
+          extra_groups = {}, -- table: additional groups that should be cleared
+          exclude_groups = {}, -- table: groups you don't want to clear
+        })
+      end,
+    },
     {
       "CRAG666/code_runner.nvim",
       keys = {

eeb1d754e5ed5060f96ef76ebafaf581475a56f9
pu-007 <pu.007@qq.com>
2024-08-09 10:15:53 +0800

    update dashboard

--- lua/plugins/general.lua
+++ lua/plugins/general.lua
@@ -19,11 +19,11 @@ return {
     config = function()
       require("accelerated-jk").setup({
         mode = "time_driven",
-        enable_deceleration = false,
+        enable_deceleration = true,
         acceleration_motions = { "w", "b" },
         acceleration_limit = 150,
         acceleration_table = { 7, 12, 17, 21, 24, 26, 28, 30 },
-        deceleration_table = { { 150, 9999 } },
+        deceleration_table = { { 200, 3 }, { 300, 7 }, { 450, 11 }, { 600, 15 }, { 750, 21 }, { 900, 9999 } },
       })
     end,
   },
@@ -78,4 +78,53 @@ return {
   },
   { "rescript-lang/vim-rescript", ft = "rescript" },
   { "ActivityWatch/aw-watcher-vim" },
+  {
+    "nvimdev/dashboard-nvim",
+    lazy = false, -- As https://github.com/nvimdev/dashboard-nvim/pull/450, dashboard-nvim shouldn't be lazy-loaded to properly handle stdin.
+    config = function()
+      require("dashboard").setup({
+        theme = "hyper",
+        config = {
+          shortcut = {
+            {
+              action = "lua LazyVim.pick()()",
+              desc = " Find File",
+              icon = " ",
+              key = "1",
+            },
+            {
+              action = "FzfLua live_grep",
+              desc = " Find Text",
+              icon = " ",
+              key = "2",
+            },
+            {
+              action = "ene | startinsert",
+              desc = " New File",
+              icon = " ",
+              key = "3",
+            },
+            {
+              action = 'lua require("persistence").load()',
+              desc = " Restore Session",
+              icon = " ",
+              key = "4",
+            },
+            {
+              action = function()
+                vim.api.nvim_input("<cmd>qa<cr>")
+              end,
+              desc = " Quit",
+              icon = " ",
+              key = "q",
+            },
+          },
+          mru = { limit = 10, icon = "  ", label = "Recent Files", cwd_only = false },
+          project = { enable = true, limit = 3, icon = "  ", label = "Projects", action = "FzfLua files cwd=" },
+          packages = { enable = false },
+          footer = {},
+        },
+      })
+    end,
+  },
 }

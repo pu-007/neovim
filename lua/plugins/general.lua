92ec9bd64b516fbf98649265a40dcdd11c2ba687
pu-007 <pu.007@qq.com>
2024-10-02 10:26:30 +0800

    update neo codium

--- lua/plugins/general.lua
+++ lua/plugins/general.lua
@@ -35,6 +35,45 @@ return {
     },
   },
   -- Code
+  -- add this to the file where you setup your other plugins:
+  {
+    "monkoose/neocodeium",
+    event = "VeryLazy",
+    config = function()
+      local neocodeium = require("neocodeium")
+
+      neocodeium.setup({
+        manual = true, -- recommended to not conflict with nvim-cmp
+      })
+
+      -- create an autocommand which closes cmp when ai completions are displayed
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "NeoCodeiumCompletionDisplayed",
+        callback = function()
+          require("cmp").abort()
+        end,
+      })
+
+      vim.keymap.set("i", "<A-f>", function()
+        require("neocodeium").accept()
+      end)
+      vim.keymap.set("i", "<A-w>", function()
+        require("neocodeium").accept_word()
+      end)
+      vim.keymap.set("i", "<A-a>", function()
+        require("neocodeium").accept_line()
+      end)
+      vim.keymap.set("i", "<A-e>", function()
+        require("neocodeium").cycle_or_complete()
+      end)
+      vim.keymap.set("i", "<A-r>", function()
+        require("neocodeium").cycle_or_complete(-1)
+      end)
+      vim.keymap.set("i", "<A-c>", function()
+        require("neocodeium").clear()
+      end)
+    end,
+  },
   {
     "stevearc/conform.nvim",
     opts = {

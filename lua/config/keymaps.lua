c60a91329dc42a4ef404444806f5e6d4799ca795
Zion Pu <pu.007@qq.com>
2025-01-11 11:55:32 +0800

    update

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,7 +6,6 @@ local map = LazyVim.safe_keymap_set
 
 map("i", "<C-v>", "<C-r><C-p>+", { desc = "Paste in Insert Mode" })
 
-if vim.loop.os_uname().sysname == "Linux" then
-  vim.api.nvim_set_keymap("n", "j", "<Plug>(accelerated_jk_gj)", {})
-  vim.api.nvim_set_keymap("n", "k", "<Plug>(accelerated_jk_gk)", {})
-end
+-- I like using H and L for beginning/end of line
+vim.keymap.set("n", "H", "^", { desc = "First non-whitespace character" })
+vim.keymap.set("n", "L", "$", { desc = "Last non-whitespace character" })
